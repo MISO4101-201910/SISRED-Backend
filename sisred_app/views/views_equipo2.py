2f10307dd6f89c91d93dd4af9d76579f664fb142
Dayana <ingrid.romero.m@gmail.com>
2019-05-15 11:00:09 -0500

    no message

--- sisred_app/views/views_equipo2.py
+++ sisred_app/views/views_equipo2.py
@@ -315,7 +315,7 @@ class ProyectoREDSerializer(serializers.ModelSerializer):
         fields= '__all__'
 
 
-#Obtiene la lista de Proyectos RED asociados al RED
+#Obtiene la lista de Proyectos RED asociados al RED. Con autenticación de usuario
 @csrf_exempt
 @api_view(["GET"])
 @permission_classes((AllowAny,))
