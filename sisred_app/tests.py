a92f4b487415529fde5e23d6a33fafdf7a789f32
ramiro.vargassalas <ramiro.vargassalas@gmail.com>
2019-05-20 21:04:31 -0500

    updated test

--- sisred_app/tests.py
+++ sisred_app/tests.py
@@ -38,6 +38,25 @@ class sisred_appTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(versionMainAfter.es_final, True)
 
+    def testMarcarComoVersionListaJustOne(self):
+        url1 = '/api/version-lista/'
+        url2 = '/marcar'
+        versionId = "1"
+        url = url1 + versionId + url2
+
+        fecha = datetime.datetime.now()
+        proyecto = ProyectoConectate.objects.create(id=1, fecha_inicio=fecha, fecha_fin=fecha)
+        red = RED.objects.create(id=1, proyecto_conectate=proyecto)
+        versionMain = Version.objects.create(id=1, es_final=False, numero=1, red=red)
+
+        response = self.client.post(url, format='json')
+
+        versionMainAfter = Version.objects.get(id=1)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(versionMainAfter.es_lista, True)
+
+
     def testMarcarComoVersionFinalFirstMark(self):
         url1 = '/api/versiones/'
         url2 = '/marcar'
@@ -59,6 +78,27 @@ class sisred_appTestCase(TestCase):
         self.assertEqual(versionMainAfter1.es_final, False)
         self.assertEqual(versionMainAfter2.es_final, True)
 
+    def testMarcarComoVersionFinalFirstMark(self):
+        url1 = '/api/version-lista/'
+        url2 = '/marcar'
+        versionId = "2"
+        url = url1 + versionId + url2
+
+        fecha = datetime.datetime.now()
+        proyecto = ProyectoConectate.objects.create(id=1, fecha_inicio=fecha, fecha_fin=fecha)
+        red = RED.objects.create(id=1, proyecto_conectate=proyecto)
+        versionMain = Version.objects.create(id=2, es_final=False, numero=2, red=red)
+        versionMain = Version.objects.create(id=1, es_final=False, numero=1, red=red)
+
+        response = self.client.post(url, format='json')
+
+        versionMainAfter1 = Version.objects.get(id=1)
+        versionMainAfter2 = Version.objects.get(id=2)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(versionMainAfter1.es_lista, False)
+        self.assertEqual(versionMainAfter2.es_lista, True)
+
     def testMarcarComoVersionFinalSecondMark(self):
         url1 = '/api/versiones/'
         url2 = '/marcar'
@@ -79,6 +119,8 @@ class sisred_appTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(versionMainAfter1.es_final, False)
         self.assertEqual(versionMainAfter2.es_final, True)
+
+
 
     def testBuscarRedNameAllParameters(self):
         fecha = datetime.datetime.now()
