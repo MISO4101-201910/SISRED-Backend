19a8add2415b4cd39973b7b6ceea485d516b22f9
vivasoft <vargaspintoadriana@gmail.com>
2019-05-20 21:22:19 -0500

    Update views_equipo1.py

--- sisred_app/views/views_equipo1.py
+++ sisred_app/views/views_equipo1.py
@@ -195,3 +195,171 @@ def sincronizarFases(idRed, idActual, idFase):
     print(response)
 
     return Response(response)
+
+#Autor:         Adriana Vargas
+#Fecha:         2019-05-09
+#Parametros:    contenido -> Comentario de cierre
+#               version -> Versión a la que pertenece el comentario
+#               usuario -> Usuario que realizó el comentario
+#               comentario_multimedia -> Id de la tabla ComentarioMultimedia
+#               fecha_creacion -> fecha en que se realizó el comentario
+#               esCierre -> Bandera para identificar si el comentario es de cierre o no
+#Descripcion:   Funcionalidad para almacenar el comentario de cierre en un archivo de PDF
+
+@api_view(['POST'])
+def comentario_cierre_post(request):
+    '''token = request.META['HTTP_AUTHORIZATION']
+    token = token.replace('Token ', '')
+    try:
+        TokenStatus = Token.objects.get(key=token).user.is_active
+    except Token.DoesNotExist:
+        TokenStatus = False
+    if TokenStatus == True:
+        reqUser = Token.objects.get(key=token).user.id'''
+
+    reqUser = 1
+
+    if request.method == 'POST':
+        data = json.loads(request.body)
+
+        contenido = data['contenido']
+        version = Version.objects.get(id=data['version'])
+        usuario = Perfil.objects.get(usuario__id=reqUser)
+        comentario_multimedia = ComentarioMultimedia.objects.get(id=data['comentario_multimedia'])
+        fecha_creacion = datetime.now()
+        esCierre = data['esCierre']
+
+        comentario = Comentario.objects.create(
+            contenido=contenido,
+            version=version,
+            usuario=usuario,
+            comentario_multimedia=comentario_multimedia,
+            fecha_creacion=fecha_creacion,
+            esCierre=esCierre
+        )
+        comentario.save()
+
+        serializer=ComentarioCierreSerializer(comentario, many=False)
+
+        return JsonResponse(serializer.data, safe=True)
+    return HttpResponseNotFound()
+
+#Autor:         Adriana Vargas
+#Fecha:         2019-05-09
+#Parametros:    id -> Id de comentario multimedia
+#               cerrado -> Identificador para determinar si el comentario fue cerrado
+#               resuelto -> Identificador para determinar si el comentario fue resuelto
+#Descripcion:   Funcionalidad para actualizar los estados del comentario base una vez ha sido cerrado
+
+@api_view(['PUT'])
+def comentario_cierre_put(request, pk):
+        if request.method == 'PUT':
+
+                comentario = json.loads(request.body)
+                comentario_base = Comentario.objects.filter(comentario_multimedia=pk).earliest('fecha_creacion')
+
+                comentario_base.cerrado = comentario['cerrado']
+                comentario_base.resuelto = comentario['resuelto']
+                comentario_base.save()
+
+                serializer = ComentarioCierreSerializer(comentario_base, many=False)
+
+                return JsonResponse(serializer.data, safe=True)
+        return HttpResponseNotFound()
+
+# Autor:         Adriana Vargas
+# Fecha:         2019-05-09
+# Parametros:    id -> Identificador del comentario multimedia
+# Descripcion:   Funcionalidad para obtener el comentario base a partir de un comentario del hilo
+
+@api_view(['GET'])
+def comentario_base_get(request,pk):
+
+    comentario_base = Comentario.objects.filter(comentario_multimedia=pk).earliest('fecha_creacion')
+
+    if(comentario_base==None):
+        raise NotFound(detail="Error 404, Comment not found", code=404)
+    serializer = ComentarioCierreSerializer(comentario_base)
+    return Response(serializer.data)
+
+
+#Autor: Francisco Perneth
+#Fecha: 2019-05-09
+#Parametros:
+#    Request -> Datos de la solicitud
+#    id -> id de la version
+#Descripcion:
+#   Permite consultar una version del RED mediante su identificador (id)
+
+@api_view(['GET'])
+def comentario_pdf_get(request,pk):
+    comentarios = Comentario.objects.filter(version=pk).order_by("id")
+    if(len(comentarios)==0):
+        vers=Version.objects.filter(id=pk)
+        if (vers != None):
+            Resp = VersionSerializer(vers[0])
+            return Response(Resp.data)
+        else:
+            raise NotFound(detail="Error 404, recurso not found", code=404)
+    else:
+        filtered = [x for x in comentarios if x.EsPadre]
+        serializer = ComentariosPDFSerializer(filtered, many=True)
+        return Response(serializer.data)
+
+#Autor:         Adriana Vargas
+#Fecha:         2019-05-10
+#Parametros:    contenido -> Comentario
+#               version -> Versión a la que pertenece el comentario
+#               usuario -> Usuario que realizó el comentario
+#               comentario_multimedia -> Id de la tabla ComentarioMultimedia
+#               fecha_creacion -> fecha en que se realizó el comentario
+#Descripcion:   Funcionalidad para guardar un comentario en un archivo de PDF
+
+@api_view(['POST'])
+def comentario_pdf_post(request):
+    '''token = request.META['HTTP_AUTHORIZATION']
+    token = token.replace('Token ', '')
+    try:
+        TokenStatus = Token.objects.get(key=token).user.is_active
+    except Token.DoesNotExist:
+        TokenStatus = False
+    if TokenStatus == True:
+        reqUser = Token.objects.get(key=token).user.id'''
+
+    reqUser = 1
+
+    if request.method == 'POST':
+        data = json.loads(request.body)
+
+        contenido = data['contenido']
+        version = Version.objects.get(id=data['version'])
+        usuario = Perfil.objects.get(usuario__id=reqUser)
+        fecha_creacion = datetime.now()
+
+        try:
+            comentario_multimedia = ComentarioMultimedia.objects.get(id=data['comentario_multimedia'])
+        except ComentarioMultimedia.DoesNotExist:
+            comentario_multimedia = None
+
+        if comentario_multimedia == None:
+
+            x1 = data['x1']
+            x2 = data['x2']
+            y1 = data['y1']
+            y2 = data['y2']
+
+            comentario_multimedia = ComentarioMultimedia.objects.create(x1=x1, y1=y1, x2=x2, y2=y2)
+
+        comentario = Comentario.objects.create(
+            contenido=contenido,
+            version=version,
+            usuario=usuario,
+            comentario_multimedia=comentario_multimedia,
+            fecha_creacion=fecha_creacion
+        )
+        comentario.save()
+
+        serializer=comentariosHijosSerializer(comentario, many=False)
+
+        return JsonResponse(serializer.data, safe=True)
+    return HttpResponseNotFound()
